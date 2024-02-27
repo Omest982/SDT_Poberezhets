59698f10bb53864c28639dd8ebac4644a6c3294f
Ivan <omest982@gmail.com>
2024-02-27 12:44:40 +0200

    Refactored Task2.

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -4,28 +4,28 @@ import java.util.*;
 
 public class Task2 {
     public static void main(String[] args) {
-        Character character1 = first_non_repeating_letter("stress");
+        char character1 = first_non_repeating_letter("stress");
         System.out.println(character1);
-        Character character2 = first_non_repeating_letter("sTreSS");
+        char character2 = first_non_repeating_letter("sTreSS");
         System.out.println(character2);
-        Character character3 = first_non_repeating_letter("gegewesade");
+        char character3 = first_non_repeating_letter("gegewesade");
         System.out.println(character3);
-        Character character4 = first_non_repeating_letter("sss");
+        char character4 = first_non_repeating_letter("sss");
         System.out.println(character4);
     }
 
-    public static Character first_non_repeating_letter(String str){
+    public static char first_non_repeating_letter(String str){
         Map<Character, Integer> map = new LinkedHashMap<>();
         for (int i = 0; i < str.length(); i++){
-            Character currentChar = str.charAt(i);
-            Character lowChar = Character.toLowerCase(currentChar);
-            Character highChar = Character.toUpperCase(currentChar);
+            char currentChar = str.charAt(i);
+            char lowChar = Character.toLowerCase(currentChar);
+            char highChar = Character.toUpperCase(currentChar);
             if (map.containsKey(lowChar)){
-                Integer val = map.get(lowChar);
+                int val = map.get(lowChar);
                 map.put(lowChar, ++val);
             }
             else if (map.containsKey(highChar)){
-                Integer val = map.get(highChar);
+                int val = map.get(highChar);
                 map.put(highChar, ++val);
             }
             else {
